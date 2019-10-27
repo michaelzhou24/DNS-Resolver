3581dca863c68e5e5f075bdea5853c45004c7cab
Michael Zhou <36398173+michaelzhou24@users.noreply.github.com>
2019-10-27 11:37:35 -0700

    fixed identations, added sendquery

--- DNSlookup.java
+++ DNSlookup.java
@@ -1,11 +1,14 @@
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 /**
- * 
+ *
  */
 
 /**
@@ -14,58 +17,70 @@ import java.util.Scanner;
  * Feel free to modify and rearrange code as you see fit
  */
 public class DNSlookup {
-    
-    
-    static final int MIN_PERMITTED_ARGUMENT_COUNT = 2;
-	static final int MAX_PERMITTED_ARGUMENT_COUNT = 3;
-	public static DatagramSocket socket;
 
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) throws Exception {
-	String fqdn;
-	DNSResponse response; // Just to force compilation
-	int argCount = args.length;
-	boolean tracingOn = false;
-	boolean IPV6Query = false;
-	InetAddress rootNameServer;
-	String UserInput;
-	
-	if (argCount < MIN_PERMITTED_ARGUMENT_COUNT || argCount > MAX_PERMITTED_ARGUMENT_COUNT) {
-	    usage();
-	    return;
+	private static final int MIN_PERMITTED_ARGUMENT_COUNT = 2;
+	private static final int MAX_PERMITTED_ARGUMENT_COUNT = 3;
+	private static final int DNS_SERVER_PORT = 53;
+	private static DatagramSocket socket;
+	private static String fqdn;
+	private static DNSResponse response; // Just to force compilation
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+
+		int argCount = args.length;
+		boolean tracingOn = false;
+		boolean IPV6Query = false;
+
+		InetAddress rootNameServer;
+		String UserInput;
+
+		if (argCount < MIN_PERMITTED_ARGUMENT_COUNT || argCount > MAX_PERMITTED_ARGUMENT_COUNT) {
+			usage();
+			return;
+		}
+
+		rootNameServer = InetAddress.getByName(args[0]);
+		fqdn = args[1];
+
+		if (argCount == 3) {  // option provided
+			if (args[2].equals("-t"))
+				tracingOn = true;
+			else if (args[2].equals("-6"))
+				IPV6Query = true;
+			else if (args[2].equals("-t6")) {
+				tracingOn = true;
+				IPV6Query = true;
+			} else  { // option present but wasn't valid option
+				usage();
+				return;
+			}
+		}
+
+
+		socket = new DatagramSocket();
+		Scanner in = new Scanner(System.in);
+		System.out.println("DNSLOOKUP> ");
+		UserInput = in.nextLine();
+		String[] commandArgs = UserInput.split(" ");
+		if (commandArgs[0].equalsIgnoreCase("lookup")){
+			//look up
+			findAndPrintTrace(commandArgs[1]);
+		}
 	}
-	
-	rootNameServer = InetAddress.getByName(args[0]);
-	fqdn = args[1];
-	
-	if (argCount == 3) {  // option provided
-	    if (args[2].equals("-t"))
-		tracingOn = true;
-	    else if (args[2].equals("-6"))
-		IPV6Query = true;
-	    else if (args[2].equals("-t6")) {
-		tracingOn = true;
-		IPV6Query = true;
-	    } else  { // option present but wasn't valid option
-		usage();
-		return;
-	    }
-	}
 
-
-	socket = new DatagramSocket();
-	Scanner in = new Scanner(System.in);
-	System.out.println("DNSLOOKUP> ");
-	UserInput = in.nextLine();
-	String[] commandArgs = commandLine.split(" ");
-	if (commandArgs[0].equalsIgnoreCase("lookup")){
-		//look up
-		findAndPrintTrace(commandArgs[1]);
+	private static void sendQuery(byte[] frame, InetAddress rootNameServer) {
+		DatagramPacket dnsReqPacket = new DatagramPacket(frame, frame.length, rootNameServer, DNS_SERVER_PORT);
+		try {
+			socket.send(dnsReqPacket);
+		} catch (IOException e) {
+			System.err.println("Failed to send DNS request.");
+			e.printStackTrace();
+		}
 	}
-    }
 
 	// Start adding code here to initiate the lookup
 	private static void findAndPrintTrace(String domainName){
@@ -102,31 +117,31 @@ public class DNSlookup {
 			String label = labels[i];
 			queryBuffer[ptr++] = (byte) label.length();
 			for (char c : label.toCharArray()) {
-			  queryBuffer[ptr++] = (byte) ((int) c);
+				queryBuffer[ptr++] = (byte) ((int) c);
 			}
-		  }
-		  queryBuffer[ptr++] = (byte) 0; //end of QNAME
-		  int QTYPE = node.getType().getCode();
-		  queryBuffer[ptr++] = (byte) ((QTYPE >>> 8) & 0xff);
-		  queryBuffer[ptr++] = (byte) (QTYPE & 0xff);
-		  int QCLASS = 1; // always Internet(IN)
-		  queryBuffer[ptr++] = (byte) 0;
-		  queryBuffer[ptr++] = (byte) QCLASS;
-		  return Arrays.copyOfRange(queryBuffer, 0, ptr);
 		}
+		queryBuffer[ptr++] = (byte) 0; //end of QNAME
+		int QTYPE = node.getType().getCode();
+		queryBuffer[ptr++] = (byte) ((QTYPE >>> 8) & 0xff);
+		queryBuffer[ptr++] = (byte) (QTYPE & 0xff);
+		int QCLASS = 1; // always Internet(IN)
+		queryBuffer[ptr++] = (byte) 0;
+		queryBuffer[ptr++] = (byte) QCLASS;
+		return Arrays.copyOfRange(queryBuffer, 0, ptr);
+	}
 
 
-	
-    
-    private static void usage() {
-	System.out.println("Usage: java -jar DNSlookup.jar rootDNS name [-6|-t|t6]");
-	System.out.println("   where");
-	System.out.println("       rootDNS - the IP address (in dotted form) of the root");
-	System.out.println("                 DNS server you are to start your search at");
-	System.out.println("       name    - fully qualified domain name to lookup");
-	System.out.println("       -6      - return an IPV6 address");
-	System.out.println("       -t      - trace the queries made and responses received");
-	System.out.println("       -t6     - trace the queries made, responses received and return an IPV6 address");
-    }
+
+
+	private static void usage() {
+		System.out.println("Usage: java -jar DNSlookup.jar rootDNS name [-6|-t|t6]");
+		System.out.println("   where");
+		System.out.println("       rootDNS - the IP address (in dotted form) of the root");
+		System.out.println("                 DNS server you are to start your search at");
+		System.out.println("       name    - fully qualified domain name to lookup");
+		System.out.println("       -6      - return an IPV6 address");
+		System.out.println("       -t      - trace the queries made and responses received");
+		System.out.println("       -t6     - trace the queries made, responses received and return an IPV6 address");
+	}
 }
 
