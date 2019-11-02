ec1f7d5be3ea81fc74651c17b8802f44f42ac650
Zane Z <lucianzhang1998@gmail.com>
2019-11-02 12:20:54 -0700

    transfer decoderesponse

--- DNSResponse.java
+++ DNSResponse.java
@@ -1,9 +1,5 @@
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.IOError;
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.InetAddress;
 
 // Lots of the action associated with handling a DNS query is processing 
 // the response. Although not required you might find the following skeleton of
@@ -64,44 +60,116 @@ public class DNSResponse {
 
 	    // Extract list of answers, name server, and additional information response 
 	    // records
-        readResponse();
+        decodeResponse(buf);
 	}
 
-    public void readResponse() throws IOException {
-        DataInputStream din = new DataInputStream(new ByteArrayInputStream(buf));
+//    public void readResponse() throws IOException {
+//        DataInputStream din = new DataInputStream(new ByteArrayInputStream(buf));
+//
+//        queryID = (int) din.readShort();
+//        System.out.println(String.format("%x", queryID));
+//        short flags = din.readShort();
+//        System.out.println(String.format("%x", flags));
+//        // get info from flags
+//        // verify response
+//        String s = Integer.toBinaryString(flags).substring(16,32);
+//        System.out.println(s);
+//
+//
+//        short questions = din.readShort();
+//        System.out.println(String.format("%x", questions));
+//        answerCount = (int) din.readShort();
+//        System.out.println(String.format("%x", answerCount));
+//        short numAuth = din.readShort();
+//        System.out.println(String.format("%x", numAuth));
+//        short numAdditional = din.readShort();
+//        System.out.println(String.format("%x", numAdditional));
+//
+//        // find fdqn
+//        StringBuilder responseFdqn = new StringBuilder();
+//        int recLen = 0;
+//        while ((recLen = din.readByte()) > 0) {
+//            byte[] record = new byte[recLen];
+//            for (int i = 0; i < recLen; i++) {
+//                record[i] = din.readByte();
+//            }
+//            responseFdqn.append(new String(record, "UTF-8"));
+//            responseFdqn.append('.');
+//        }
+//        responseFdqn.setLength(responseFdqn.length() - 1);
+//        System.out.println(responseFdqn.toString());
+//    }
 
-        queryID = (int) din.readShort();
-        System.out.println(String.format("%x", queryID));
-        short flags = din.readShort();
-        System.out.println(String.format("%x", flags));
-        // get info from flags
-        // verify response
-        String s = Integer.toBinaryString(flags).substring(16,32);
-        System.out.println(s);
-        
+    // Decode response header
+    private static void decodeResponse(byte[] responseBuffer) {
+        int pointer = 0;
+        int responseID = TwoByteToInt(responseBuffer[0], responseBuffer[1]);
+        int QR = (responseBuffer[2] & 0x80) >>> 7; // get 1st bit
+        int opCode = (responseBuffer[2] & 0x78) >>> 3; // get 2nd, 3rd, 4th and 5th bit
+        int AA = (responseBuffer[2] & 0x04) >>> 2; // geth 6th
+        int TC = (responseBuffer[2] & 0x02) >>> 1; // get 7th bit
+        int RD = responseBuffer[2] & 0x01; // get 8th bit
+        int RA = responseBuffer[3] & 0x80;
+        int RCODE = responseBuffer[3] & 0x0F;
+        System.out.println("responseID: " + responseID);
+        System.out.println("QR: " + QR);
+        System.out.println("opCode: " + opCode);
+        System.out.println("AA: " + AA);
+        System.out.println("TC: " + TC);
+        System.out.println("RD: " + RD);
+        System.out.println("RA: " + RA);
+        System.out.println("RCODE: " + RCODE);
 
-        short questions = din.readShort();
-        System.out.println(String.format("%x", questions));
-        answerCount = (int) din.readShort();
-        System.out.println(String.format("%x", answerCount));
-        short numAuth = din.readShort();
-        System.out.println(String.format("%x", numAuth));
-        short numAdditional = din.readShort();
-        System.out.println(String.format("%x", numAdditional));
-
-        // find fdqn
-        StringBuilder responseFdqn = new StringBuilder();
-        int recLen = 0;
-        while ((recLen = din.readByte()) > 0) {
-            byte[] record = new byte[recLen];
-            for (int i = 0; i < recLen; i++) {
-                record[i] = din.readByte();
-            }
-            responseFdqn.append(new String(record, "UTF-8"));
-            responseFdqn.append('.');
+        String message = "";
+        switch (RCODE) {
+            case 0:
+                message = "No error condition";
+                break;
+            case 1:
+                message = "FAILED. Format error, Name server can't interpret query";
+                break;
+            case 2:
+                message = "FAILED. Name server error";
+                break;
+            case 3:
+                message = "FAILED. Name error – name doesnot exist";
+                break;
+            case 4:
+                message = "FAILED. Requested quet type not supported";
+                break;
+            case 5:
+                message = "FAILED. Request refused";
+                break;
+            default:
+                message = "FAILED. Unknown RCODE";
+                break;
         }
-        responseFdqn.setLength(responseFdqn.length() - 1);
-        System.out.println(responseFdqn.toString());
+        System.out.println(message);
+
+        int QDCOUNT = TwoByteToInt(responseBuffer[4], responseBuffer[5]);
+        int ANCOUNT = TwoByteToInt(responseBuffer[6], responseBuffer[7]);
+        int NSCOUNT = TwoByteToInt(responseBuffer[8], responseBuffer[9]);
+        int ARCOUNT = TwoByteToInt(responseBuffer[10], responseBuffer[11]);
+
+        System.out.println("QDCOUNT: " + QDCOUNT);
+        System.out.println("ANCOUNT: " + ANCOUNT);
+        System.out.println("NSCOUNT: " + NSCOUNT);
+        System.out.println("ARCOUNT: " + ARCOUNT);
+
+
+        // requires work on translating hex into char
+
+        int QTYPE = TwoByteToInt(responseBuffer[pointer++], responseBuffer[pointer++]);
+        int QCLASS = TwoByteToInt(responseBuffer[pointer++], responseBuffer[pointer++]);
+
+        System.out.println("QTYPE: " + QTYPE);
+        System.out.println("QCLASS: " + QCLASS);
+
+        System.out.println("  Answers (" + ANCOUNT + ")");
+        for (int i = 0; i < ANCOUNT; i++) {
+            // requires method decoding each one of the RR received
+        }
+
     }
 
 
@@ -112,5 +180,20 @@ public class DNSResponse {
     // You will also want methods to extract the response records and record
     // the important values they are returning. Note that an IPV6 reponse record
     // is of type 28. It probably wouldn't hurt to have a response record class to hold
-    // these records. 
+    // these records.
+
+
+
+    // ---------------------------------------- Helper Functions
+    // -------------------------------------------
+
+    // 2 byte -> int
+    private static int TwoByteToInt(byte b1, byte b2) {
+        return ((b1 & 0xFF) << 8) + (b2 & 0xFF);
+    }
+
+    // 4 byte -> int
+    private static int FoutByteToInt(byte b1, byte b2, byte b3, byte b4) {
+        return ((b1 & 0xFF) << 24) + ((b2 & 0xFF) << 16) + ((b3 & 0xFF) << 8) + (b4 & 0xFF);
+    }
 }
--- DNSlookup.java
+++ DNSlookup.java
@@ -2,9 +2,6 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
 
 /**
  *
@@ -22,16 +19,15 @@ public class DNSlookup {
     private static DatagramSocket socket;
     private static String fqdn;
     private static DNSResponse response; // Just to force compilation
-    private static int argCount = args.length;
     private static boolean tracingOn = false;
     private static boolean IPV6Query = false;
-    private static int pointer = 0; // pointer when decoding messages
+//    private static int pointer = 0; // pointer when decoding messages
 
     /**
      * @param args
      */
     public static void main(String[] args) throws Exception {
-
+        int argCount = args.length;
         // int argCount = args.length;
         // boolean tracingOn = false;
         // boolean IPV6Query = false;
@@ -60,16 +56,8 @@ public class DNSlookup {
                 return;
             }
         }
-
-        socket = new DatagramSocket();
-        Scanner in = new Scanner(System.in);
-        System.out.println("DNSLOOKUP> ");
-        UserInput = in.nextLine();
-        String[] commandArgs = UserInput.split(" ");
-        if (commandArgs[0].equalsIgnoreCase("lookup")) {
-            // look up
-            findAndPrintTrace(commandArgs[1]);
-        }
+        DNSQuery query = new DNSQuery(fqdn, rootNameServer, tracingOn, IPV6Query);
+        String result = query.query(rootNameServer);
     }
 
     private static void sendQuery(byte[] frame, InetAddress rootNameServer) {
@@ -88,124 +76,53 @@ public class DNSlookup {
     }
 
     // Encode query as a message following the doamin protocal
-    private static byte[] encodeQuery(int queryID) {
-        byte[] queryBuffer = new byte[512];
-        int thirdByte = queryID >>> 8;
-        int forthByte = queryID & 0xff;
-        queryBuffer[0] = (byte) thirdByte;
-        queryBuffer[1] = (byte) forthByte;
-        int QROpcodeAATCRD = 0; // 0 iterative, 1 recursive
-        queryBuffer[2] = (byte) QROpcodeAATCRD;
-        int RAZRCODE = 0;
-        queryBuffer[3] = (byte) RAZRCODE;
-        int QDCOUNT = 1;
-        queryBuffer[4] = (byte) 0;
-        queryBuffer[5] = (byte) QDCOUNT;
-        int ANCOUNT = 0;
-        queryBuffer[6] = (byte) 0;
-        queryBuffer[7] = (byte) ANCOUNT;
-        int NSCOUNT = 0;
-        queryBuffer[8] = (byte) 0;
-        queryBuffer[9] = (byte) NSCOUNT;
-        int ARCOUNT = 0;
-        queryBuffer[10] = (byte) 0;
-        queryBuffer[11] = (byte) ARCOUNT;
-        int ptr = 12;
-        String[] labels = fqdn.split("\\.");
-        for (int i = 0; i < labels.length; i++) {
-            String label = labels[i];
-            queryBuffer[ptr++] = (byte) label.length();
-            for (char c : label.toCharArray()) {
-                queryBuffer[ptr++] = (byte) ((int) c);
-            }
-        }
-        queryBuffer[ptr++] = (byte) 0; // end of QNAME
-        // encode query type
-        // int QTYPE;
-        // if (IPV6Query = true){
-        // QTYPE = 1;
-        // } else {QTYPE = 1;}
-        queryBuffer[ptr++] = (byte) ((QTYPE >>> 8) & 0xff);
-        queryBuffer[ptr++] = (byte) (QTYPE & 0xff);
-        int QCLASS = 1; // always Internet(IN)
-        queryBuffer[ptr++] = (byte) 0;
-        queryBuffer[ptr++] = (byte) QCLASS;
-        return Arrays.copyOfRange(queryBuffer, 0, ptr);
-    }
-
-    // Decode response header
-    private static void decodeResponse(byte[] responseBuffer) {
-        int responseID = TwoByteToInt(responseBuffer[0], responseBuffer[1]);
-        int QR = (responseBuffer[2] & 0x80) >>> 7; // get 1st bit
-        int opCode = (responseBuffer[2] & 0x78) >>> 3; // get 2nd, 3rd, 4th and 5th bit
-        int AA = (responseBuffer[2] & 0x04) >>> 2; // geth 6th
-        int TC = (responseBuffer[2] & 0x02) >>> 1; // get 7th bit
-        int RD = responseBuffer[2] & 0x01; // get 8th bit
-        int RA = responseBuffer[3] & 0x80;
-        int RCODE = responseBuffer[3] & 0x0F;
-        System.out.println("responseID: " + responseID);
-        System.out.println("QR: " + QR);
-        System.out.println("opCode: " + opCode);
-        System.out.println("AA: " + AA);
-        System.out.println("TC: " + TC);
-        System.out.println("RD: " + RD);
-        System.out.println("RA: " + RA);
-        System.out.println("RCODE: " + RCODE);
-
-        String message = "";
-        switch (RCODE) {
-        case 0:
-            message = "No error condition";
-            break;
-        case 1:
-            message = "FAILED. Format error, Name server can't interpret query";
-            break;
-        case 2:
-            message = "FAILED. Name server error";
-            break;
-        case 3:
-            message = "FAILED. Name error – name doesnot exist";
-            break;
-        case 4:
-            message = "FAILED. Requested quet type not supported";
-            break;
-        case 5:
-            message = "FAILED. Request refused";
-            break;
-        default:
-            message = "FAILED. Unknown RCODE";
-            break;
-        }
-
-        int QDCOUNT = TwoByteToInt(responseBuffer[4], responseBuffer[5]);
-        int ANCOUNT = TwoByteToInt(responseBuffer[6], responseBuffer[7]);
-        int NSCOUNT = TwoByteToInt(responseBuffer[8], responseBuffer[9]);
-        int ARCOUNT = TwoByteToInt(responseBuffer[10], responseBuffer[11]);
-
-        System.out.println("QDCOUNT: " + QDCOUNT);
-        System.out.println("ANCOUNT: " + ANCOUNT);
-        System.out.println("NSCOUNT: " + NSCOUNT);
-        System.out.println("ARCOUNT: " + ARCOUNT);
+//    private static byte[] encodeQuery(int queryID) {
+//        byte[] queryBuffer = new byte[512];
+//        int thirdByte = queryID >>> 8;
+//        int forthByte = queryID & 0xff;
+//        queryBuffer[0] = (byte) thirdByte;
+//        queryBuffer[1] = (byte) forthByte;
+//        int QROpcodeAATCRD = 0; // 0 iterative, 1 recursive
+//        queryBuffer[2] = (byte) QROpcodeAATCRD;
+//        int RAZRCODE = 0;
+//        queryBuffer[3] = (byte) RAZRCODE;
+//        int QDCOUNT = 1;
+//        queryBuffer[4] = (byte) 0;
+//        queryBuffer[5] = (byte) QDCOUNT;
+//        int ANCOUNT = 0;
+//        queryBuffer[6] = (byte) 0;
+//        queryBuffer[7] = (byte) ANCOUNT;
+//        int NSCOUNT = 0;
+//        queryBuffer[8] = (byte) 0;
+//        queryBuffer[9] = (byte) NSCOUNT;
+//        int ARCOUNT = 0;
+//        queryBuffer[10] = (byte) 0;
+//        queryBuffer[11] = (byte) ARCOUNT;
+//        int ptr = 12;
+//        String[] labels = fqdn.split("\\.");
+//        for (int i = 0; i < labels.length; i++) {
+//            String label = labels[i];
+//            queryBuffer[ptr++] = (byte) label.length();
+//            for (char c : label.toCharArray()) {
+//                queryBuffer[ptr++] = (byte) ((int) c);
+//            }
+//        }
+//        queryBuffer[ptr++] = (byte) 0; // end of QNAME
+//         // encode query type
+//         int QTYPE;
+//         if (IPV6Query = true){
+//         QTYPE = 1;
+//         } else {QTYPE = 1;}
+//        queryBuffer[ptr++] = (byte) ((QTYPE >>> 8) & 0xff);
+//        queryBuffer[ptr++] = (byte) (QTYPE & 0xff);
+//        int QCLASS = 1; // always Internet(IN)
+//        queryBuffer[ptr++] = (byte) 0;
+//        queryBuffer[ptr++] = (byte) QCLASS;
+//        return Arrays.copyOfRange(queryBuffer, 0, ptr);
+//    }
 
 
-        // requires work on translating hex into char
-
-        int QTYPE = TwoByteToInt(responseBuffer[pointer++], responseBuffer[pointer++]);
-        int QCLASS = TwoByteToInt(responseBuffer[pointer++], responseBuffer[pointer++]);
-
-        System.out.println("QTYPE: " + QTYPE);
-        System.out.println("QCLASS: " + QCLASS);
-
-        System.out.println("  Answers (" + ANCOUNT + ")");
-        for (int i = 0; i < ANCOUNT; i++) {
-            // requires method decoding each one of the RR received
-        }
-
-    }
-
-
-
-    private static decodeOneRR(byte[] responseBuffer){
+    private static void decodeOneRR(byte[] responseBuffer){
         //
     }
 
